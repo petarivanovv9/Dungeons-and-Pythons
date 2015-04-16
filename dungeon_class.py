f703aa21bd62e484fb039eeac2e634bc6307f1bf
Petar Ivanov <pepincho95@gmail.com>
2015-04-16 19:56:28 +0300

    adding more things to the dungeon class

--- dungeon_class.py
+++ dungeon_class.py
@@ -1,19 +1,36 @@
+from hero_class import Hero
+
+
 class Dungeon:
 
     def __init__(self, filename):
         with open(filename, "r") as in_file:
-            self.dungeon_map = in_file.read().splitlines()
+            lines = in_file.read().splitlines()
+            self.dungeon_map = []
+            for line in lines:
+                self.dungeon_map.append(list(line))
 
     def print_map(self):
         for line in self.dungeon_map:
-            print (line)
+            print (''.join(line))
 
     def spawn(self, hero):
-        pass
+        counter = 0
+        for i in range(0, len(self.dungeon_map)):
+            for j in range(0, len(self.dungeon_map[i])):
+                if self.dungeon_map[i][j] == "S" and counter < 1:
+                    counter += 1
+                    self.dungeon_map[i][j] = "H"
+
 
     def move_hero(self, position="None"):
         pass
 
 
+h = Hero(name="Bron", title="Dragonslayer", health=100, mana=100, mana_regeneration_rate=2)
+
 dungeon_map = Dungeon("level1")
+dungeon_map.print_map()
+dungeon_map.spawn(h)
+print (10 * '-')
 dungeon_map.print_map()
